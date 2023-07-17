e41652709f1da772aeb57d12e8c877a8e048a66e
Hammed Tosin <hammedtosin188@gmail.com>
2023-07-17 01:24:12 +0000

    refactored

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,14 +1,11 @@
 export default function handleResponseFromAPI(promise) {
   return promise
-    .then(() => {
+    .then(() => ({
+      status: 200,
+      body: 'success',
+    }))
+    .catch(() => (new Error()))
+    .finally(() => {
       console.log('Got a response from the API to the console');
-      return {
-        status: 200,
-        body: 'success',
-      };
-    })
-    .catch(() => {
-      console.log('Got a response from the API to the console');
-      return (new Error());
     });
 }
