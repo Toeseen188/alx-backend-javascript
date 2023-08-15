d27ac109bcaf980b0fcd33f2bca383bc76ecdb72
Hammed Tosin <hammedtosin188@gmail.com>
2023-08-15 16:46:02 +0000

    destructure array return from promise.all explicitly

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,10 +2,11 @@ import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()])
-    .then(([uploadPhotoResult, createUserResult]) => {
+    .then((results) => {
+      const [uploadPhotoResult, createUserResult] = results;
       console.log(uploadPhotoResult.body, createUserResult.firstName, createUserResult.lastName);
     })
     .catch(() => {
-      console.log('Signup system offline');
+      console.error('Signup system offline');
     });
 }
