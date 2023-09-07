2d541b0b9ef2879e71da4010dc6729ee06802d9c
Hammed Tosin <hammedtosin188@gmail.com>
2023-09-07 21:27:19 +0000

    await, async, Promise, try, catch and throw

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,19 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default async function asyncUploadUser() {
+  const [uploadValue, createValue] = await Promise.all([uploadPhoto(), createUser()]);
+
+  try {
+    const obj = {
+      photo: uploadValue,
+      user: createValue,
+    };
+    return obj;
+  } catch (error) {
+    const obj = {
+      photo: null,
+      user: null,
+    };
+    return obj;
+  }
+}
