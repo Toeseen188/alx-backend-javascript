a1495d15b2aca6aba1d384ede935a30066e6f150
Hammed <hammedtosin188@gmail.com>
2024-02-05 15:45:42 +0000

    first testing

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -4,21 +4,21 @@ const calculateNumber = require('./0-calcul.js');
 describe("calculateNumber", function() {
   it("should round up and add two numbers", function() {
     const result = calculateNumber(1, 3);
-    assert.equal(result, 4);
+    assert.strictEqual(result, 4);
   });
 
   it("should round up and add mixed integer and floating numbers", function(){
     const result = calculateNumber(1, 3.7);
-    assert.equal(result, 5);
+    assert.strictEqual(result, 5);
   });
 
   it("should round up, add floating-point numbers", function(){
     const result = calculateNumber(1.2, 3.7);
-    assert.equal(result, 5);
+    assert.strictEqual(result, 5);
   });
 
   it("should round up and two floating-point numbers", function(){
     const result = calculateNumber(1.5, 3.7);
-    assert.equal(result, 6);
+    assert.strictEqual(result, 6);
   });
 });
